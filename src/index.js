5793d266e95e3ece170a7cbcabf5165e0b18f121
Tyler <tyler_venetsanos@msn.com>
2021-04-14 12:47:53 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+}); 
+
+const newTaskDescription = document.getElementById("create-task-form")
+console.log(newTaskDescription)
+newTaskDescription.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = document.getElementById("tasks")
+  const newTask = document.createElement("li")
+  console.log(event)
+newTask.innerText = event.target[0].value;
+  task.appendChild(newTask)
 });
+
